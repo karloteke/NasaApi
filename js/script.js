71ad3dcd994c7dcd14e0d5b41b65555c5321d949
Carlota Cetina <carlotika26@gmail.com>
2023-03-29 00:25:42 +0100

    new navigation flux

--- js/script.js
+++ js/script.js
@@ -6,8 +6,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
 	}
 	/* Método para hacer la petición a la API y obtener el json de resultados */
 	const getList = async () => {
-		const url = 'https://api.nasa.gov/planetary/apod?api_key=eSrdcJqzrhfKIcvAYj29MEXKZFJGdDTLLn01sXnI&start_date=2023-03-01&end_date=2023-03-27';
-
+		const url = fillUrlParams();
 		const result = await fetch(url);
 		elementsList = await result.json();
 		document.querySelector('#search').style.display = 'block';
@@ -17,20 +16,30 @@ window.addEventListener('DOMContentLoaded', (event) => {
 	fetchList();
 });
 
+const fillUrlParams = () => {
+	const today = new Date();
+	const dateDesde = new Date( new Date().setDate(new Date().getDate() - 30));
+	console.log(dateDesde);
+	const hasta = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDay();
+	const desde = dateDesde.getFullYear() + '-' + (dateDesde.getMonth() + 1) + '-' + dateDesde.getDay();
+	const url = `https://api.nasa.gov/planetary/apod?api_key=eSrdcJqzrhfKIcvAYj29MEXKZFJGdDTLLn01sXnI&start_date=${desde}&end_date=${hasta}`;
+	return url;
+}
+
 const createItems = (items) => {
 	// console.log('Creating items...');
 	const list = document.querySelector('#pod_list');
 	list.innerHTML = '';
 	const newElement = document.createElement('div');
-	items?.forEach((item, pos) => {
+	items?.forEach((item) => {
 		const itemHtml = `
 			<div class="list-element flex">
 				<div>
 					<h2>${item.title}</h2>
 					<p>Desc: ${trimDescription(item.explanation)}</p>
 					<div>Date: ${item.date}</div>
-					<button onclick="openDetail(${pos})">Detail</button>
-				</div>	
+					<a href="detail.html?date=${item.date}">Detail</a>
+				</div>
 				<div>
 					<img src="${item.url}">
 				</div>	
@@ -42,30 +51,6 @@ const createItems = (items) => {
 
 	list.appendChild(newElement);
 }
-
-const openDetail = (index) => {
-	// console.log('Abrimos el detalle de: ', elementsList[index]);
-	document.querySelector('#bt').style.display = 'block';
-	document.querySelector('#search').style.display = 'none';
-	const list = document.querySelector('#pod_list');
-	list.innerHTML = '';
-	const detailsData = elementsList[index];
-
-	const newElement = document.createElement('div');
-	const itemHtml = `
-		<div class="list-detail flex">
-			<h2>${detailsData.title}</h2>
-			<img src="${detailsData.url}">
-			<p>Desc: ${detailsData.explanation}</p>
-			<div>Date: ${detailsData.date}</div>	
-			<div>Autor: ${detailsData.copyright}</div>	
-		</div>
-	`;
-
-	newElement.innerHTML = itemHtml;
-
-	list.appendChild(newElement);
-};
 
 const filterElements = () => {
 	let valor = document.querySelector('#search-input').value;
@@ -84,16 +69,11 @@ const filterElements = () => {
 		}
 	}
 };
+
 /*Cortamos descripción a partir de 250 carácteres para verla entera en OpenDetails*/
 const trimDescription = (desc) => {
 	if (desc.length > 250) {
 		return desc.substring(0, 250) + ' ...';
 	}
 	return desc;
-};
-
-const goBack = () => {
-	document.querySelector('#bt').style.display = 'none';	
-	document.querySelector('#search').style.display = 'block';
-	createItems(elementsList);
 };
